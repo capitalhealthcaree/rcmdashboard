b8a0ce82dbef437e47d664e98d85f7ad255f45d8
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-05-04 00:56:19 -0700

    add name

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -21,7 +21,7 @@ const SideBar = ({ isOpen, toggle }) => (
       <span color="info" onClick={toggle} style={{ color: "#fff" }}>
         &times;
       </span>
-      <h3>Premier Pain</h3>
+      <h3>mymedicalbillingrcm</h3>
     </div>
     <div className="side-menu">
       <Nav vertical className="list-unstyled pb-3">

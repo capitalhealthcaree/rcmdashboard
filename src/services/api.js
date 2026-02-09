231a6dd4b6d5bd3ac87b3f5f8e259a8921051bce
capitalhealthcaree <premierpatientappointment@gmail.com>
2026-02-10 02:55:53 +0500

    api url update

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://rcmbackend.onrender.com/";
+const BASE_URL = "https://rcmbackend-a-pi.vercel.app/";
 const LOCAL_URL = "http://localhost:5000";
 const config = {
   headers: {
